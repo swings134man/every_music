ae8ebdeacb9b8cf806d02113f17e7a1bd77fc7ae
Seok Jun Kang <swings134@gmail.com>
2022-03-27 18:52:58 +0900

    swagger web.xml path 변경 -> servlet-context 내부에 존

--- every_music/src/main/java/com/modu/everymusic/config/SwaggerConfig.java
+++ every_music/src/main/java/com/modu/everymusic/config/SwaggerConfig.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -20,7 +19,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@EnableWebMvc
 public class SwaggerConfig {
 
 	private static final String API_NAME = "Every_Music";
